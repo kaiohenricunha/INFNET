e04aead7c64a7ef6f25e1f4816456ebdd2fec560
kaiohenricunha <kaio.cunha@al.infnet.edu.br>
2021-11-01 12:20:47 -0300

    update on etapa4exe5

--- bloco2/PB/etapa4/etapa4 - Exercício5.py
+++ bloco2/PB/etapa4/etapa4 - Exercício5.py
@@ -1,6 +1,8 @@
 import os
 lista = os.listdir()
 dic_arq = {} # Usar dicionário para guardar os arquivos por tipo
+lista_dir = [] # Usar lista para guardar os diretórios
+
 for i in lista:
     if os.path.isfile(i):
         ext = os.path.splitext(i)[1] # Separa em nome e extensão
@@ -8,6 +10,8 @@ for i in lista:
         if not ext in dic_arq:
             dic_arq[ext] = []
         dic_arq[ext].append(i) # Usa a extensão como chave
+    else:
+        lista_dir.append(i)
 
 print(dic_arq)
 
@@ -16,7 +20,7 @@ for i in dic_arq:
     for j in dic_arq[i]:
         print("\t"+j)
     print("")
-if len(lista) > 0:
+if len(lista_dir) > 0:
     print("Diretórios:")
     for i in lista:
         print("\t"+i)
