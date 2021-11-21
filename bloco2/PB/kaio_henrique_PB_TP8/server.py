6f287cb31eed4962834c63d8d9fd860cc1987d1e
kaiohenricunha <kaio.cunha@al.infnet.edu.br>
2021-11-20 21:06:35 -0300

    added some features to the server

--- bloco2/PB/kaio_henrique_PB_TP8/server.py
+++ bloco2/PB/kaio_henrique_PB_TP8/server.py
@@ -1,9 +1,9 @@
 # TP8
 # Server
 
-import socket
+import socket, random
 
-HOST = '192.168.0.206'
+HOST = socket.gethostbyname(socket.gethostname())
 PORT = 9090
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,11 +11,25 @@ server.bind((HOST, PORT))
 
 server.listen(5)
 
+print("Server named", HOST, "is listening on port", PORT)
+
+communication_socket, address = server.accept()
+
 while True:
-    communication_socket, address = server.accept()
-    print('Conected to', address)
-    message = communication_socket.recv(1024).decode('utf-8')
-    print('Message from client is:', message)
-    communication_socket.send('Got your message! Thank you!'.encode('utf-8'))
-    communication_socket.close()
-    print('Connection with', address, 'closed')
+    msg = communication_socket.recv(1024)
+    if '$' == msg.decode('utf-8'):
+        print('Connection with', address, 'closed')
+        communication_socket.close()
+        break
+    elif '?' in msg.decode('utf-8'):
+        resp = str(random.randint(0, 1))
+        msg = "Yes\n"
+        if resp == '0':
+            msg = "No\n"
+        else:
+            msg = "Ok..." + msg.decode('utf-8')
+            communication_socket.send(msg.encode('utf-8'))
+
+server.close()
+input("Press any key to exit...") 
+    
