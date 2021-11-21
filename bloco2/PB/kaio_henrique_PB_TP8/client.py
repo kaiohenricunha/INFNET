ea518ff3b9f9590fd8472a5fd71da2964213587a
kaiohenricunha <kaio.cunha@al.infnet.edu.br>
2021-11-21 20:33:33 -0300

    updated the client server logic

--- bloco2/PB/kaio_henrique_PB_TP8/client.py
+++ bloco2/PB/kaio_henrique_PB_TP8/client.py
@@ -1,26 +1,51 @@
-# TP 8
-# Client
-
 import socket, sys
 
-HOST = '127.0.0.1'
-PORT = 9090
+HOST = "127.0.0.1"
+PORT = 9999
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def server_staggered_scheeduling():
+    client.connect((HOST, PORT))
+    client.send("1".encode('utf-8'))
+    response = client.recv(4096)
+    print("\n")
+    print("Listing scheduled functions on the server: ", HOST, PORT)
+    print(response.decode('utf-8'))
+    print("\n")
+    client.close()
 
-try:
-    socket.connect((HOST, PORT))
-except Exception as error:
-    print('Error:', error)
-    sys.exit()
+def get_network_interfaces():
+    client.connect((HOST, PORT))
+    client.send("2".encode('utf-8'))
+    response = client.recv(4096)
+    print("\n")
+    print("Listing available network interfaces on the server: " , HOST, PORT)
+    print(response.decode('utf-8'))
+    print("\n")
+    client.close()
 
-print("To exit, type '$'")
-msg = input()
-socket.send(msg.encode('utf-8'))
-while msg != '$':
-    msg = socket.recv(1024)
-    print(msg.decode('utf-8'))
-    msg = input()
-    socket.send(msg.encode('utf-8'))
+def menu():
+    print("""
+    1 - Run scheduled functions
+    2 - List available network interfaces
+    3 - Exit
+    """)
 
-socket.close()
+def main():
+    menu()
+    while True:
+        try:
+            choice = int(input("Choose an option: "))
+            if choice == 1:
+                server_staggered_scheeduling()
+            elif choice == 2:
+                get_network_interfaces()
+            elif choice == 3:
+                sys.exit()
+            else:
+                print("Invalid option")
+        except ValueError:
+            print("Invalid option")
+
+if __name__ == "__main__":
+    sys.exit(main())
