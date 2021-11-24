157181721dddf62017133da4f40bc57678b7b754
kaiohenricunha <kaio.cunha@al.infnet.edu.br>
2021-11-23 23:51:12 -0300

    updates on tp9

---
+++ bloco2/PB/kaio_henrique_PB_TP9/client.py
@@ -0,0 +1,156 @@
+import cpuinfo, datetime, sched, time, platform, os, nmap, subprocess, socket, pickle,psutil
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = socket.gethostname() 
+PORT = 9999
+HP = (HOST, PORT)
+buffer = 1024
+
+def one():
+    list = []
+
+    print("\Files\n")
+    d1 = response[1]
+    for key, d1 in d1.items():
+        print(key)
+        for attribute,value in d1.items():
+            print('{}: {}'.format(attribute,value))
+        print("")
+
+    time.sleep(8)
+    os.system('clear') 
+    list.append(key)
+    return list
+
+def two():
+    list = []
+    print("\n2- Processes | PID\n")
+    title_pid = ['Name:', 'PID:', 'Executable:', 'CPU(s):', 'Memory(MB):', ]               
+    i = 0
+
+    while i <= len(response):
+        pid_info = [response[i][0], response[i][1], response[i][2], response[i][3], response[i][4]]
+        i = i + 1                  
+        for x,y in zip(title_pid,pid_info):
+            print ('{:30}{:<30}'.format(x,y))
+        print("")
+
+    time.sleep(8)
+    os.system('clear') 
+    list.append(title_pid)
+    return list
+
+def three():
+    list = []
+
+    print("\n3 - Scheduled Functions\n")
+    d1 = response[1]
+    d2 = response[0]
+
+    
+    print(d1, d2)
+
+    time.sleep(8)
+    os.system('clear') 
+    list.append(d1)
+    list.append(d2)
+    return list
+
+def four():
+    print("\n6- Up Hosts\n")
+    list = []
+    nmap_info = ['NMAP INFO']
+    i = 0
+
+    while i <= len(response):
+        nmap_info1 = [response[i][0]]
+        i = i + 1
+        for x,y in zip(nmap_info, nmap_info1):
+            print('{:30}{:<30}'.format(x,y))
+        print("")
+
+    time.sleep(8)
+    os.system('clear') 
+    list.append(nmap_info)
+    return list
+
+def five():
+    list = []
+    print("\n6- Ports\n")
+    port_info = ['PORT INFO']
+    i = 0
+
+    while i <= len(response):
+        port_info1 = [response[i][0]]
+        i = i + 1
+        for x,y in zip(port_info, port_info1):
+            print('{:30}{:<30}'.format(x,y))
+        print("")
+
+    time.sleep(8)
+    os.system('cls')
+    list.append(port_info)
+    return list
+
+def six():
+    list = []
+    print("\n6- Network\n")
+    title_network = ['IPv4:', 'IPv6:', 'Mask:', 'MAC:']
+    valor_network = [response[0], response[1], response[2], response[3]]
+
+    for x,y in zip(title_network,valor_network):
+        print ('{:30}{:30}'.format(x,y))
+
+    time.sleep(8)
+    os.system('clear') 
+    list.append(title_network)
+    return list
+
+
+def menu():
+    print('============================')
+    print('OPTIONS')
+    print('============================')
+    print('|1| File Info')
+    print('|2| Processes | PID')
+    print('|3| Scheduled Functions')
+    print('|4| Up Hosts')
+    print('|5| Ports')
+    print('|6| Network')
+    print('============================') 
+
+try: 
+    client.connect(HP)
+    option = " "
+    client.send(option.encode('utf-8')) 
+    options = ("1","2", "3", "4", "5", "6")
+    
+    count = 1
+    while count == 1:   
+        menu()
+        option = input('Type in your option: ')
+        if (option in options):
+            client.send(option.encode('utf-8'))
+            bytes_resp = client.recv(buffer)
+            response = pickle.loads(bytes_resp)          
+            if option == "1":
+                one()           
+            elif option == "2":
+                two()       
+            elif option == "3":
+                three()                         
+            elif option == "4":  
+                four()                                    
+            elif option == "5":
+                five()                 
+            elif option == "6":
+                six()
+            elif option == '7':
+                count == 2
+            else:
+                print('Invalid option. Try again...')              
+except Exception as erro:
+    print(str(erro))
+
+client.close()
+input("Press any key to exit...")
---
+++ bloco2/PB/kaio_henrique_PB_TP9/server.py
@@ -0,0 +1,168 @@
+import psutil, cpuinfo, datetime, time, platform, os, nmap, subprocess, socket, pickle, sched
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = socket.gethostname() 
+PORT = 9999
+HP = (HOST, PORT)
+buffer = 1024
+
+server.bind(HP)
+server.listen()
+print("Server:", HOST, "waiting on the port", PORT, "Just a minute...")
+(client, addr) = server.accept()
+print("Client connected:", addr)
+
+# TP4
+
+def get_file():    
+    start = time.time()
+    dir = os.listdir() 
+    dir_list = [] 
+    dic_file_data = {}   
+    d2 = {} 
+    for file in dir: 
+        if os.path.isfile(file): 
+            size=(round(os.stat(file).st_size / 1024)) # file size
+            path=os.path.abspath(file) # file path
+            filec=time.ctime(os.path.getctime(file)) # creation time
+            filem=time.ctime(os.path.getmtime(file)) # last modification time
+            filea=time.ctime(os.path.getatime(file)) # last access time
+            d2={'Size (bytes)':size,'Path':path,'Criação':filec,'Last modification':filem,'Last access':filea} 
+            dic_file_data.update({file:d2}) 
+        else: 
+            dir_list.append(file) 
+    list = [dir_list, dic_file_data]    
+    
+    end = time.time()
+    print("Time spent:", round((end-start),2), "second(s).")  
+    return list
+
+def pid():   
+    start = time.time()   
+    dic = {}
+    key = 0
+    
+    for proc in psutil.process_iter():
+        if proc.name() == 'python3':   
+            processID = proc.pid        
+            try:            
+                processName = proc.name()
+                processExecutable = proc.exe()
+                processCpuTime = round(proc.cpu_times().user,2)
+                processMemory = round((proc.memory_info().rss / (1024*1024)),2)             
+                
+                list = []
+                list.append(processName)
+                list.append(processID)
+                list.append(processExecutable) 
+                list.append(processCpuTime)
+                list.append(processMemory) 
+
+                dic[key] = list
+                key = key + 1            
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess): 
+               pass   
+    end = time.time()
+    print("Time spent:", round((end-start),2), "second(s).\n")    
+    return dic
+
+# TP5
+
+def scheduler():
+    start = time.time()
+    scheduler = sched.scheduler(time.time, time.sleep)
+
+    # enter: delay, priority, function name, arguments tuple
+    scheduler.enter(2, 1, get_file, ())
+    scheduler.run()
+    end_file = time.time()
+
+    scheduler.enter(3, 1, pid, ())
+    scheduler.run()
+    end_pid = time.time()
+
+    duration_file = abs(end_file - start) # abs() returns an absolute value
+    duration_pid = abs(end_pid - start)
+
+    lista_info = []
+    print("Finishing the scheduled functions...")
+    info1 = (f" Time spent: {duration_file:.0f} seconds")
+    info2 = (f" Time spent: {duration_pid:.0f} seconds")
+    lista_info.append(info1)
+    lista_info.append(info2)
+
+    end = time.time()
+    print("Total time spent:", round((end-start),2), "second(s).")  
+    return lista_info
+
+# TP6
+
+def up_hosts():
+    start = time.time()
+    up_hosts = os.system(f"nmap -sP  {HOST}")
+
+    end = time.time()
+    print("Time spent:", round((end-start),2), "second(s).")  
+    return up_hosts
+
+def port_nmap():
+    start = time.time()
+    ports = os.system(f"nmap -p- {HOST}")
+
+    end = time.time()
+    print("Time spent:", round((end-start),2), "second(s).")  
+    return ports
+
+# TP7
+
+def network():
+    start = time.time()
+
+    interfaces = psutil.net_if_addrs()
+    names = []
+
+    for i in interfaces:
+        names.append(str(i))
+        dic_itf = psutil.net_if_addrs()
+        
+        list = []
+        ipv4 = dic_itf[names[0]][1][1]
+        ipv6 = dic_itf[names[0]][2][1]
+        mask = dic_itf[names[0]][1][2]
+        mac = dic_itf[names[0]][0][1]
+
+        list.append(ipv4)
+        list.append(ipv6)
+        list.append(mask)
+        list.append(mac)       
+        
+    end = time.time()
+    print("Time spent:", round((end-start),2), "seconds(s).")    
+    return list
+
+while True:
+    option = client.recv(buffer)   
+    if option.decode('utf-8') == " ":
+        print("Connected.")   
+    elif option.decode('utf-8') == "1":
+        bytes_resp = pickle.dumps(get_file())
+        client.send(bytes_resp)
+    elif option.decode('utf-8') == "2":
+        bytes_resp = pickle.dumps(pid())
+        client.send(bytes_resp)      
+    elif option.decode('utf-8') == "3":
+        bytes_resp = pickle.dumps(scheduler())
+        client.send(bytes_resp)   
+    elif option.decode('utf-8') == "4":
+        bytes_resp = pickle.dumps(up_hosts())
+        client.send(bytes_resp)      
+    elif option.decode('utf-8') == "5":
+        bytes_resp = pickle.dumps(port_nmap())
+        client.send(bytes_resp)      
+    elif option.decode('utf-8') == "6":
+        bytes_resp = pickle.dumps(network())
+        client.send(bytes_resp)
+
+client.close()
+server.close()
+input("Press any key to exit...")
